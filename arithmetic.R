67582447a1b59e75edc218da6c0e0204eb2dc136
simon <simonzhang0428@gmail.com>
2020-08-12 23:50:48 -0700

    arithmetic

---
+++ arithmetic.R
@@ -0,0 +1,23 @@
+x <- 7 + 4 - 5 * 2
+12 - 2 / 3
+23 %% 3 # remainder
+23 %/% 3 # floor div
+(23 %/% 3) * 3 + 23 %% 3 == 23
+2 ^ 3
+1.5 ^ 2
+9 ^ .5
+27 ^ (1/3)
+exp(0)
+exp(1)
+log(exp(2))
+log10(1000)
+log(49, base = 7)
+abs(-2)
+sqrt(4)
+sqrt(2)
+pi
+sin(pi/6)
+cos(pi/3)
+tan(pi/4)
+atan(1) 
+pi / 4
